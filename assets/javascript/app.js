cb37f38983efac3c5088cd1dee732a7571a3787d
Danielle Bowman <Danielle@Danielles-MacBook-Air-2.local>
2016-05-08 20:34:18 -0400

    images show

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,49 +1,9 @@
+$(document).ready(function(){
+
 // Initial array of search
-	var gifs = ['Dwight', 'Jim Halpert', 'Michael Scott'];
-
-	// ========================================================
-
-	// Generic function for dumping the JSON content for each button into the div
-	function displayGifs(){
-
-var gifName = $(this).data("name");
-	var queryURL = "'http://api.giphy.com/v1/gifs/search?q="+ gifName + "&api_key=dc6zaTOxFJmzC";
-
-		
-  
-
-		$.ajax({url: queryURL, method: 'GET'}).done(function(response) {
-	     	 // Creates a generic div to hold the gif
-			var gifDiv = $('<div class="movie">');
-
-			// Retrieves the rating
-			var rating = response.Rating;
-
-			// Creates an element to have the rating displayed
-			var pOne = $('<p>').text( "Rating: " + rating);
-
-			// Displays the rating
-			gifDiv.append(pOne);
-
-			
-
-		
-			// Creates an element to hold the mp4 
-			var image = $('<img>').attr("images/fixed_height/mp4", response.mp4);
-
-			// Appends the mp4
-			gifDiv.append(image);
-
-			
-			
-		});
-
-	}
 	
+	var gifs = ['Law and Order', 'SVU', 'Olivia Benson'];
 
-	
-
-	
 
 	// function to generate gif data
 	function renderButtons(){ 
@@ -60,39 +20,86 @@ var gifName = $(this).data("name");
 		    var a = $('<button>')
 		    a.addClass('gif'); // Added a class 
 		    a.attr('data-name', gifs[i]); // Added a data-attribute
+		    a.addClass('gifButton'); 
 		    a.text(gifs[i]); // Provided the initial button text
 		    $('#gifView').append(a); // Added the button to the HTML
 		}
 	}
+	renderButtons();
+
+	// ========================================================
+$('#addGif').on('click', function(){
+
+		// This line of code will grab the input from the textbox
+		var gif = $('#gif-input').val().trim();
+
+		// The movie from the textbox is then added to our array
+		gifs.push(gif);
+		
+		// Our array then runs which handles the processing of our movie array
+		renderButtons();
+
+		// We have this line so that users can hit "enter" instead of clicking on ht button and it won't move to the next page
+		return false;
+	})
+	
+
+	
+
+	
 
 	// ========================================================
 
 	// Events where one is clicked
-	$('#addGif').on('click', function(){
+	 $('#gifView').on('click','.gifButton',function() {
+        var p = $(this).data('name');
+        var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + p + "&api_key=dc6zaTOxFJmzC&limit=10";
 
-		// Grabs the input from the textbox
-		var gif = $('#gif-input').val().trim();
+        $.ajax({
+                url: queryURL,
+                method: 'GET'
+            })
+            .done(function(response) {
+             console.log(response);
+             var results = response.data;
+                for (var i = 0; i < results.length; i++) {
+                    var gifDiv = $('<div class="item">')
 
-		// The input from the textbox is added to array
-		gifs.push(gif);
-		
-		// Our array then runs 
-		renderButtons();
+                    var rating = results[i].rating;
 
- 
-		return false;
-	})
+                    var p = $('<p>').text("Rating: " + rating);
+
+                    var gifImage = $('<img>').attr('src', results[i].images.fixed_height_still.url);
+                    gifImage.attr('data-still',results[i].images.fixed_height_still.url);
+                    gifImage.attr('data-animate', results[i].images.fixed_height.url);
+                    gifImage.attr('data-state', 'still');
+                    gifImage.addClass('workingGif');
+
+
+                    gifDiv.append(p)
+                    gifDiv.append(gifImage)
+
+                    $('#gifShow').prepend(gifDiv);
+                }
+
+            });
+    });
+	
+	$('#gifShow').on('click', 'workingGif', function(){
+		if (state == 'still'){
+			$(this).attr('src', 'animate');
+			$(this).attr('data-state', 'animate');
+		}
+		else
+		{
+			$(this).attr('src', 'still');
+			$(this).attr('data-state', 'still');
+		}
+	});
+
 
 	// ========================================================
-
-	// Generic function for displaying the gifInfo
-	$(document).on('click', '.gif', displayGifs);
-
-
-	// ========================================================
-
-	// This calls the renderButtons() function
-	renderButtons();
+});
 
 
 
